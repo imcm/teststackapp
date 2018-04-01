e3d88653bde725d4098ae36f359afd5cb49dfe30
imcm <37277739+imcm@users.noreply.github.com>
2018-04-01 16:42:55 -0400

    Update incoming-form.php file
    Add Insert statement on form submit using mysqli

--- inventory-app/incoming-form.php
+++ inventory-app/incoming-form.php
@@ -3,18 +3,41 @@
 <h1>Add an order</h1>
 
 <form method="post">
-	<label for="first">First Name</label>
-	<input type="text" name="first" id="first">
-	<label for="last">Last Name</label>
-	<input type="text" name="last" id="last">
-	<label for="productid">Product ID</label>
-	<input type="text" name="productid" id="productid">
-	<label for="number">Number Shipped</label>
-	<input type="text" name="number" id="number>
-<input type="button" value="Submit" onclick="incoming-purchases.php"/>
-<button type="submit" formaction="incoming-purchases.php">Submit</button>
+  <label>First Name</label>
+  <input type="text" name="first" id="first" required="required"/><br><br>
+  <label>Last Name</label>
+  <input type="text" name="last" id="last" required="required"/><br><br>
+  <label>Product ID</label>
+  <input type="text" name="productid" id="productid" required="required"/><br><br>
+  <label>Quantity</label>
+  <input type="text" name="quantity" id="quantity" required="required"/><br><br>
+
+  <input type="submit" value="Submit" name="submit"/>
+
 </form>
+
 <a href="incoming-purchases.php">Back to Incoming Purchases</a>
-	</div>
+        </div>
 </head>
 
+<?php
+
+  if(isset($_POST["submit"])){
+
+    require "config.php";
+    $conn = new mysqli($host, $username, $password, $dbname);
+    if ($conn->connect_error){
+      die("Connection failed: " . $conn->connect_error);
+    }
+    $sql = "INSERT INTO orders (First, Last, ProductId, NumberShipped, OrderDate)
+    VALUES ('".$_POST["first"]."','".$_POST["last"]."','".$_POST["productid"]."','".$_POST["quantity"]."',CURDATE())";
+
+    if ($conn->query($sql) === TRUE) {
+      echo "New record created successfully";
+    } else {
+      echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+  }
+?>
